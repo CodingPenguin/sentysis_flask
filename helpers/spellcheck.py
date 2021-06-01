58a833255537ce391eb34802e0877ac8280c1cd5
CodingPenguin <nguyenhuydanny@gmail.com>
2021-05-31 21:19:49 -0500

    Fixed spellcheck.py so that it actually spellchecks

--- helpers/spellcheck.py
+++ helpers/spellcheck.py
@@ -1,9 +1,6 @@
-import re
+import html
 from textblob import TextBlob
 
 def spellcheck(value):
-    replace_dict = {"\xa0": " ", "&#39;": "'", "&quot;": '"', "\u003cbr /\u003e": " ", "\r": "" }
-    rep = dict((re.escape(k), v) for k, v in replace_dict.items())
-    pattern = re.compile("|".join(rep.keys()))
-    value = str(TextBlob(pattern.sub(lambda m: rep[re.escape(m.group(0))], value)))
-    return value
+    result = TextBlob(html.unescape(value))
+    return result
